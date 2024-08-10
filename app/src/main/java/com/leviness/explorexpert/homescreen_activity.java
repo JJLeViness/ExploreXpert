6d2980b99a2e0de2732677eff05899ec4283b918
Grace Metallo <GeMetallo@student.fullsail.edu>
2024-08-10 13:51:07 -0400

    Splash Screen and Main Screen UI 8/10

---
+++ app/src/main/java/com/leviness/explorexpert/homescreen_activity.java
@@ -0,0 +1,15 @@
+package com.leviness.explorexpert;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class homescreen_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen);
+    }
+}
+
+
